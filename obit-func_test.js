16b4b5dcde2a10a6a9c46f8b7438366e455fdd87
Salman Malik <smalik@SalmanMcycomInc.attlocal.net>
2022-12-16 00:43:45 -0600

    wrote and ran codesept tests that opened the page, checked for the first name, filled out the Obit guestbook message - still working on the click to submit the form. having a hard time narrowing down the actual button element in the selenium webdriver

--- obit-func_test.js
+++ obit-func_test.js
@@ -5,6 +5,15 @@ Feature('obit-func');
 Scenario('Should open the page, assert that the descendants first name appears in the Name section, Scroll down until the Guest Book is visible, Leave a Guest Book entry',  ({ I }) => {
         I.amOnPage('https://www.qa-legacy.com/us/obituaries/chicagotribune/name/virginia-gruchalski-obituary?pid=196167379'); // opens main page of website
         I.see('Virginia')
+        I.fillField({name: '_GuestbookForm_Message'}, 'testing this function to see if this works')
+       
+        //FOR THE PHOTO UPLOADING SECTION oF THE OBIT MESSAGE:
+        // I.click('')    click the add the photo
+        // await I.appendFile()     placing a file in the popout modal
+       
+        I.fillField({name: '_GuestbookForm_From'}, 'John Doe')
+        I.fillField({name: '_GuestbookForm_Email'}, 'jdoe@test.com')
+        //I.click()
         pause();
 
 });
